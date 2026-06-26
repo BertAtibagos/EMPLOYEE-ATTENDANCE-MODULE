93b65fdf6d539d27160250bfde1bf0d12344e563
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-26 16:50:19 +0800

    Added a external css and adjusted classes

--- index.php
+++ index.php
@@ -4,25 +4,28 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Employee Attendance</title>
-
+    <link rel="stylesheet" href="css.css?t=<?php echo time(); ?>">
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
         rel="stylesheet"
     />
-    <link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body class="bg-light">
 
 <div class="container py-4">
-    <div class="card p-3 mb-4 shadow-sm">
-        <div class="row g-3" id="subjectFilter">
-           
-        </div>
-    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card p-3 mb-4 shadow-sm">
+                <div class="row g-3" id="subjectFilter">
+                
+                </div>
+            </div>
 
-    <div class="card p-3 shadow-sm">
-        <div class="table-responsive" id="attndnc_logs_card">
-           
+            <div class="card p-3 shadow-sm attendance-card">
+                <div id="attndnc_logs_card">
+                
+                </div>
+            </div>
         </div>
     </div>
 </div>
