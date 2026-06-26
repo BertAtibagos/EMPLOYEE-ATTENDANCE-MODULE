4a79015ff253fa0470856f3572a35c1e6aec8dfa
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-26 16:48:01 +0800

    Deleted a comment

--- components/subject-filter.js
+++ components/subject-filter.js
@@ -4,10 +4,6 @@ export async function subjectFilterComponent(){
 
     const res = await subjects();
 
-    // const options = res.map(subj =>
-    //     `<option value="${subj.subj_id}">${subj.subj_desc}</option>`
-    // ).join('');
-
     return(
         `<div class="col-md-4">
             <label class="form-label">Subject</label>
