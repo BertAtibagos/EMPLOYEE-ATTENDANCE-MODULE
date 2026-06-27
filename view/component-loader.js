9dd6bd0444936630070c776f8068e42b810ccddd
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-27 10:52:17 +0800

    inluded the selectedSubject inside of event handler

--- view/component-loader.js
+++ view/component-loader.js
@@ -14,9 +14,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         subjectFilter.innerHTML += await buttonComponent("Search");
         attendance.innerHTML = await tableComponent();
 
-        const selectedSubject = document.getElementById('subjectSelect').value
-
         document.getElementById('searchButton').addEventListener('click', async (e)=>{
+            const selectedSubject = document.getElementById('subjectSelect').value
             const dateFilterStart = document.getElementById('dateFilterStart').value;
             const dateFilterEnd = document.getElementById('dateFilterEnd').value;
 
