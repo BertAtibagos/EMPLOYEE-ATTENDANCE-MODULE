ff1855d0d06d986d8d2e44c906674b097f400fa3
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-29 15:42:33 +0800

    Added default option

--- components/subject-filter.js
+++ components/subject-filter.js
@@ -4,13 +4,15 @@ export async function subjectFilterComponent(){
 
     const res = await subjects();
 
-    return(
-        `<div class="col-md-4">
-            <label class="form-label">Subject</label>
-            <select class="form-select" id="subjectSelect">
-                ${res.map(subj => `<option value="${subj.subj_id}">${subj.subj_desc}</option>`)}
-            </select>
-        </div>`
-    );
+    const html = `<div class="col-md-4">
+                    <label class="form-label">Subject</label>
+                    <select class="form-select" id="subjectSelect">
+                        <option value="">All Subjects</option>
+                        ${res.map(subj => `
+                            <option value="${subj.subj_id}">${subj.subj_desc}</option>
+                        `).join("")}
+                    </select>
+                </div>`
 
+    return html;
 }
